4fa56f52017c2c02ec87f085d3096fd3fb2ac253
Sergey Velykokhatko <sergey@velykokhatko.de>
2021-09-08 11:56:35 +0200

    Working UART state

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cargo build --release
+cargo build --release # --verbose
 arm-none-eabi-objcopy -O binary target/thumbv7em-none-eabihf/release/stm32f429 stm32f429-disco_blink.bin
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
 
 use crate::hal::{prelude::*, stm32};
+use crate::hal::{prelude::*, serial::config::Config, serial::Serial};
+use core::fmt::Write; // for pretty formatting of the serial output
 
 pub use cortex_m::*;
 pub use cortex_m_rt::*;
@@ -36,11 +38,12 @@ fn blink(led1 : stm32f4xx_hal::gpio::GPIOG::PG0<Output<PushPull>>) {
 #[entry]
 fn main() -> ! {
 
-    let delayTime = 20_u32;
+    let delayTime = 100_u32;
     let dp = stm32::Peripherals::take().unwrap();
     let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
     let gpioa = dp.GPIOA.split();
+    let gpiod = dp.GPIOD.split();
     let gpiog = dp.GPIOG.split();
     let key = gpioa.pa0.into_pull_down_input();
     let mut led1 = gpiog.pg13.into_push_pull_output();
@@ -53,22 +56,33 @@ fn main() -> ! {
     // Create a delay abstraction based on SysTick
     let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
 
+    // define RX/TX pins
+    let tx_pin = gpiod.pd5.into_alternate_af7();
+    let rx_pin = gpioa.pa3.into_alternate_af7();
+    // configure serial
+    let serial = Serial::usart2(
+        dp.USART2,
+        (tx_pin, rx_pin),
+        Config::default().baudrate(115_200.bps()),
+        clocks,
+    )
+    .unwrap();
+    let (mut tx, mut _rx) = serial.split();
+    let mut value: u8 = 0;
+
 
     loop {
-//        blink(led1);
-
         led1.toggle().unwrap();
-//        led1.set_high().unwrap();
         delay.delay_ms(delayTime);
-//        led1.set_low().unwrap();
-//        delay.delay_ms(delayTime);
 
         if key.is_low().unwrap() {
             led2.set_low().unwrap();
-//            let _ = led2.set_low();
         } else {
             led2.set_high().unwrap();
         }
-//        cortex_m::asm::wfi();
+
+        // print some value every 500 ms, value will overflow after 255
+        writeln!(tx, "Hello from rust in STM32F429Discovery board. Value: {:02}\r\n", value).unwrap();
+        value += 1;
     }
 }
